09f4470dff096e1915534c88b89192a396dfcf78
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-24 11:02:09 +0000

    changing color and background color using style.color, style.backgroundColor

--- clever_programmer/script.js
+++ clever_programmer/script.js
@@ -1,7 +1,15 @@
 const title = document.getElementById('title');
 const titleDiv = document.getElementById('titleDiv');
 let message = "My name is: Kayange Mwakanemela"
+
 console.log(title)
+
 title.innerText = message
+
 console.log(title.innerText)
+
 titleDiv.innerHTML = `<p>${message}</p>`
+
+titleDiv.style.color = 'blue'
+titleDiv.style.backgroundColor = 'gray'
+console.log(titleDiv.style.color)
