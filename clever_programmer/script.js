d94218b6c486c51f2e8df4bd45355a7672080630
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-24 10:31:27 +0000

    use inner html to change the element's tag

--- clever_programmer/script.js
+++ clever_programmer/script.js
@@ -1,5 +1,7 @@
 const title = document.getElementById('title');
-
+const titleDiv = document.getElementById('titleDiv');
+let message = "My name is: Kayange Mwakanemela"
 console.log(title)
-title.innerText = "Kayange Mwakanemela"
+title.innerText = message
 console.log(title.innerText)
+titleDiv.innerHTML = `<p>${message}</p>`
