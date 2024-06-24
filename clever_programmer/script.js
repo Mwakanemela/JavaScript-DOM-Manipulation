863ea71c34fb1e4a8941d9b1b0c0f741ef249a86
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-24 11:25:16 +0000

    listening for click events with onclick function

--- clever_programmer/script.js
+++ clever_programmer/script.js
@@ -1,15 +1,32 @@
 const title = document.getElementById('title');
 const titleDiv = document.getElementById('titleDiv');
+
+const blackBox = document.getElementById('black')
+const redBox = document.getElementById('red')
+const greenBox = document.getElementById('green')
+
 let message = "My name is: Kayange Mwakanemela"
 
-console.log(title)
+// console.log(title)
 
 title.innerText = message
 
-console.log(title.innerText)
+// console.log(title.innerText)
 
 titleDiv.innerHTML = `<p>${message}</p>`
 
 titleDiv.style.color = 'blue'
 titleDiv.style.backgroundColor = 'gray'
-console.log(titleDiv.style.color)
+// console.log(titleDiv.style.color)
+
+blackBox.onclick = () => {
+  console.log(`Black box clicked`)
+}
+
+redBox.onclick = () => {
+  console.log(`Red box clicked`)
+}
+
+greenBox.onclick = () => {
+  console.log(`Green box clicked`)
+}
